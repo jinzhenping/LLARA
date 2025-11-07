7e9cb6431dc5b32170b5865a5bebb94a7c03bc21
unknown <jinzhenping114@gmail.com>
2025-11-07 11:09:58 +0900

    update

--- train_mind.sh
+++ train_mind.sh
@@ -3,10 +3,10 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --data_dir data/ref/mind \
     --model_name mlp_projector \
     --loss lm \
-    --batch_size 8 \
-    --num_workers 8 \
+    --batch_size 2 \
+    --num_workers 4 \
     --lr 1e-3 \
-    --accumulate_grad_batches 8 \
+    --accumulate_grad_batches 32 \
     --max_epochs 10 \
     --check_val_every_n_epoch 1 \
     --precision bf16 \
